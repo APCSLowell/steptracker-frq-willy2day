9b76ec18dd117f3630f58e4450af1cc05be7394f
willy2day <143228693+willy2day@users.noreply.github.com>
2024-04-22 13:07:59 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -2,4 +2,24 @@ import java.util.ArrayList;
 public class StepTracker
 {
  /* to be implemented here */
+ private int steps, activeDays, days; 
+
+public StepTracker(int x){
+ steps=x; 
+ activeDays=0;
+ days=1;
+}
+public void addDailySteps (int x){
+ steps+=x;
+ days+=1;
+}
+public int activeDays(){
+ if (steps>=10000)
+   return activeDays+=1;
+}
+public double averageSteps(){
+ return (double) steps/days; 
+ 
+}
+ 
 } 
